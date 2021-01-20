9a8e622797edae2f6fd4c7a44d5548007a01fe66
Jeremy C Wu (RD-TW) <jeremyc_wu@trend.com.tw>
2021-01-20 09:39:50 +0800

    update utils data

--- utils/users.py
+++ utils/users.py
@@ -1,7 +1,7 @@
 
 
 users = [
-    {"name": "valid_user", "email": "football-good.rga48cmj@mailosaur.io", "phone": "0978126016", "password": "QWERT!@#$%", "display_name":"amazon_tester"},
+    {"name": "valid_user", "email": "football-good.rga48cmj@mailosaur.io", "phone": "0987654321", "password": "QWERT!@#$%", "display_name":"amazon_tester"},
     {"name": "invalid_user", "email": "invalidUser@test.com", "phone": "0912345678", "password": "qwert1235"},
 ]
 
